e360e859a66e25d848ce51527170fe9ba5cb5780
Carter Wu <a84682579@gmail.com>
2020-01-25 21:57:04 +0800

    add header

--- product.py
+++ product.py
@@ -10,7 +10,7 @@ while True:
 for p in products:
 	print(p[0], '的價格是$', p[1])
 
-
-
-
-
+with open('記帳練習.csv', 'w') as f:   #with可以幫你自動close檔案f
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + '$' + p[1] + '\n')
