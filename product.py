8aef2fd04207dd4e6312f4aaa3de41f2d57320d2
Carter Wu <a84682579@gmail.com>
2020-01-28 01:57:15 +0800

    refactor

--- product.py
+++ product.py
@@ -1,33 +1,51 @@
 import os #operating system
 
-#找有沒有檔案
-products = []
-if os.path.isfile('記帳練習.csv'):
-	print('yeah!')
-	with open('記帳練習.csv', 'r') as f:
+#讀取檔案
+def read_file(filename):
+	products = []
+	with open(filename, 'r') as f:
 		for line in f:
 			if '商品,價格' in line:
-			    continue #繼續，跳到下一回
+				continue #繼續，跳到下一回
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-else:
-	print('找不到檔案')
+	return products
 
 #使用者輸入
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入價格: ')
-	products.append([name, price])
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入價格: ')
+		products.append([name, price])
+	print(products)
+	return products
 
 #印出所有購買紀錄
-for p in products:
-	print(p[0], '的價格是$', p[1])
+def print_product(products):
+	for p in products:
+		print(p[0], '的價格是$', p[1])
 
 #寫入檔案
-with open('記帳練習.csv', 'w') as f:   #with可以幫你自動close檔案f
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + '$' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w') as f:   #with可以幫你自動close檔案f
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + '$' + p[1] + '\n')
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): #檢查檔案在不在
+		print('yeah!')
+		products = read_file(filename)
+	else:
+		print('找不到檔案')
+	
+	products = []
+	products = user_input(products)
+	print_product(products)
+	write_file('products.csv', products)
+
+main()
